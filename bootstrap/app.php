c5e431b1dd86b51ac28b30e1e6ac2c3d76bbaf77
Irvan Malik <irvanma@gnuweeb.org>
2025-12-09 14:48:41 +0700

    feat(bootstrap/app): global validations
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,29 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (Middleware $middleware) {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (ValidationException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => '[ERR] Validation failed.',
+                    'errors' => $e->errors(),
+                ], 422);
+            }
+            
+            return null; 
+        });
     })->create();
